0b04fa17f758560e86fad0649adfa00c0102baf4
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-05-02 16:53:12 +0200

    style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import HomePage from "./HomePage.jsx";
 
 function App() {
     return ( <
         div className = "App" >
         <
-        header className = "App-header" > < /header>{" "} <
+        header className = "App-header" >
+        <
+        HomePage / >
+        <
+        /header>{" "} <
         /div>
     );
 }
